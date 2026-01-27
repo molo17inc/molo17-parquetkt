e2e6f008746572402bf37fd1003d87e87a39f6d3
Daniele Angeli <daniele.angeli@molo17.com>
2026-01-27 11:55:23 +0100

    ci optimizations

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,13 +48,13 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
